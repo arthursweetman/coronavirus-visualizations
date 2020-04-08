fbf8a59b5ea341ff10b99e392bc3120a39f26722
Arthur Sweetman <56804555+arthursweetman@users.noreply.github.com>
2020-04-08 17:26:21 -0400

    Update

--- virus_map.R
+++ virus_map.R
@@ -1,21 +1,34 @@
 library(tidyverse)
+library(lubridate)
 library(maps)
 library(ggthemes)
 library(countrycode)
 
-# Change these file paths to match the appropriate paths on your computer
-filePath_TimeSeriesConfirmed <- "GitHub/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
-filePath_TimeSeriesDeaths <- "GitHub/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
-filePath_DailyReports <- "GitHub/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/"
-filePath_WhereToSaveTheMap <- "~/GitHub/coronavirus-visualizations"
+# Change this directory to match where you would like to save the
+# outputted maps on your computer
+filePath_WhereToSaveGlobalMap <- "~/GitHub/coronavirus-visualizations"
+filePath_WhereToSaveUSAMap <- "~/GitHub/coronavirus-visualizations"
 
-coronaCases <- read_csv(filePath_TimeSeriesConfirmed)
-coronaDeaths <- read_csv(filePath_TimeSeriesDeaths)
+# Date must be correctly formatted to correctly identify online file
+day <- ifelse(day(today()-1) < 10, 
+              paste0("0",day(today()-1)), 
+              day(today()-1))
+month <- ifelse(month(today()-1) < 10,
+                paste0("0",month(today()-1)),
+                month(today()-1))
+today <- paste(month,day,year(today()-1),sep="-")
+
+# Read in data
+daily_report <- read_csv(paste0("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/",
+                                today,".csv"))
+coronaCases <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+coronaDeaths <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 world_map <- map_data("world")
 
 # Create clone variable so we have a name to correctly reference.
 # This is necessary because the column with the most recent data
 # changes its name every day
+# Ex. Now we can refer to "recent" rather than today's date as a string
 time <- now()
 coronaCases$recent <- pull(coronaCases, ifelse(hour(time)<19, ncol(coronaCases), ncol(coronaCases)-1)) # If after 7pm, add "-1" to the end of "ncol(corona)"
 
@@ -36,7 +49,9 @@ corona_grouped <- coronaCases %>%
 # Combines map data with coronavirus data
 world_map_corona <- left_join(world_map, corona_grouped, by=c("iso3"="iso3"))
 
-# Map with a logarithmic scale
+#############################
+# World Map
+#############################
 options(scipen = 999) # Prevent scale from converting to scientific notation
 ggplot(data=world_map_corona)+
   geom_polygon(aes(x=long, y=lat, group=group, fill=recent))+
@@ -45,30 +60,23 @@ ggplot(data=world_map_corona)+
                       na.value = "gray70",
                       name="No. Confirmed",
                       trans="log",
-                      breaks=c(10,100,1000,10000,100000))+
+                      breaks=c(10,100,1000,10000,100000,1000000))+
   labs(title="Coronavirus Spread",
        subtitle = today()-1,
        caption = "Data source: https://github.com/CSSEGISandData/COVID-19")+
-  theme(plot.title = element_text(hjust = 0.5),
-        plot.subtitle = element_text(hjust = 0.5))+
+  theme(plot.title = element_text(hjust = 0.5, size=20),
+        plot.subtitle = element_text(hjust = 0.5),
+        legend.position = c(.05,.15))+
   coord_quickmap()+
-  ggsave("virus_map.png",
-         path = filePath_WhereToSaveTheMap)
+  ggsave("virus_map.png",width = 200, height=167,units="mm",
+         path = filePath_WhereToSaveGlobalMap)
 
+#############################
 # USA map with counties
-if(month(today()) < 10){
-  month <- paste0("0",month(today()-1))
-  today <- paste(month,day(today()-1),year(today()-1),sep="-")
-} else {
-  today <- paste(month(today()-1),day(today()-1),year(today()-1),sep="-")
-}
-
-# Read in Daily Report
-coronaCases1 <- read_csv(paste0(
-  filePath_DailyReports,today,".csv"))
+#############################
 
 # Do some String handling
-corona1_grouped <- coronaCases1 %>% 
+daily_report <- daily_report %>% 
   filter(Country_Region == "US") %>% 
   rename("county"=Admin2) %>% 
   mutate(Combined_Key=str_to_lower(str_sub(Combined_Key,1, -5)))
@@ -79,7 +87,7 @@ USA_map <- map_data("county") %>%
 USA_states <- map_data("state")
 
 # Combine county map and COVID-19 daily report
-USA_map_corona <- left_join(USA_map, corona1_grouped, by=c("location_full"="Combined_Key"))
+USA_map_corona <- left_join(USA_map, daily_report, by=c("location_full"="Combined_Key"))
 
 # Create map, use state map to draw state borders
 ggplot(data=USA_map_corona)+
@@ -95,8 +103,8 @@ ggplot(data=USA_map_corona)+
        subtitle = today()-1,
        caption = "Data source: https://github.com/CSSEGISandData/COVID-19")+
   theme(legend.position = c(0.85,0.05),
-        plot.title = element_text(hjust = 0.5),
+        plot.title = element_text(hjust = 0.5,size=20),
         plot.subtitle = element_text(hjust = 0.5))+
   coord_quickmap()+
-  ggsave("virus_map_USA.png",
-         path = filePath_WhereToSaveTheMap)
+  ggsave("virus_map_USA.png", width = 200, height=167,units="mm",
+         path = filePath_WhereToSaveUSAMap)
--- virus_spread.R
+++ virus_spread.R
@@ -1,15 +1,16 @@
 library(tidyverse)
 library(lubridate)
 
-# Change these file paths to match the appropriate paths on your computer
-filePath_TimeSeriesConfirmed <- "GitHub/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
-filePath_TimeSeriesDeaths <- "GitHub/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+# Change these directories to match where you would like to save the
+# outputted plots on your computer
 filePath_WhereToSaveCasesPlot <- "~/GitHub/coronavirus-visualizations"
 filePath_WhereToSaveDeathsPlot <- "~/GitHub/coronavirus-visualizations"
 
-coronaCases <- read_csv(filePath_TimeSeriesConfirmed)
-coronaDeaths <- read_csv(filePath_TimeSeriesDeaths)
+# Read in data
+coronaCases <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+coronaDeaths <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 
+# Do some data handling
 cases <- coronaCases %>% 
   gather(key = date, value = confirmed, 5:ncol(coronaCases)) %>%
   group_by(date) %>% 
@@ -22,28 +23,30 @@ deaths <- coronaDeaths %>%
   summarise(deaths=sum(deaths)) %>% 
   mutate(date=as.Date(date, format="%m/%d/%y"))
 
-# Plot of the number of confirmed cases and confirmed deaths since Jan 21, 2020.
+# Plot of the number of confirmed cases since Jan 21, 2020.
 ggplot(data=cases)+
   geom_point(aes(x=date, y=confirmed), color="red")+
   geom_line(aes(x=date, y=confirmed), color="red")+
-  geom_point(aes(x=date, y=deaths), data=deaths)+
-  geom_line(aes(x=date, y=deaths), data=deaths)+
-  labs(x="Date", y="Cumulate Number of Confirmed Cases")+
+  labs(x="Date", y="Cumulate Number of Confirmed Cases",
+       title="Cases of COVID-19")+
   theme_bw()+
   scale_x_date(date_breaks = "4 days")+
-  theme(axis.text.x = element_text(angle=90))+
-  scale_y_continuous(breaks = seq(0,2000000,50000))+
-  ggsave("virus_cases.png",
+  theme(axis.text.x = element_text(angle=90),
+        plot.title = element_text(hjust = .5))+
+  scale_y_continuous(breaks = seq(0,10000000,100000))+
+  ggsave("virus_cases.png", width=200,height=167,units="mm",
          path = filePath_WhereToSaveCasesPlot) 
 
-# Just Deaths
+# Plot of the number of confirmed deaths since Jan 21, 2020.
 ggplot(data=deaths)+
   geom_point(aes(x=date, y=deaths))+
   geom_line(aes(x=date, y=deaths))+
-  labs(x="Date", y="Cumulate Number of Deaths")+
+  labs(x="Date", y="Cumulate Number of Deaths",
+       title = "Deaths From COVID-19")+
   theme_bw()+
   scale_x_date(date_breaks = "4 days")+
-  theme(axis.text.x = element_text(angle=90))+
-  scale_y_continuous(breaks = seq(0,2000000,1000))+
-  ggsave("virus_deaths.png",
+  theme(axis.text.x = element_text(angle=90),
+        plot.title = element_text(hjust = .5))+
+  scale_y_continuous(breaks = seq(0,1000000,10000))+
+  ggsave("virus_deaths.png", width=200,height=167,units="mm",
          path = filePath_WhereToSaveDeathsPlot) 
